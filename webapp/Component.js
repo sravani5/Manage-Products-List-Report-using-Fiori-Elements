b7f2f0d32d252cadc97c6c8c7095418760ed3fa0
sravani5 <sravani.ganta555@gmail.com>
2021-06-04 10:13:51 +0530

    First Commit

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("mngprdctszprs_mng_prdcts_fe.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("mngprdctszprs_mng_prdcts_fe.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
